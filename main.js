22d20038faae1c313018cea5ce9f254be44602e1
Manuel Valls Fernández <manolo@chatiku.com>
2014-09-28 08:47:08 +0200

    Added some tests

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var Test = require('vz.test'),
     ebjs = require('ebjs'),
-    assert = require('assert');
+    vzBuffer = require('vz.buffer'),
+    assert = require('assert'),
+    
+    Stream = global.process?require('st'+'ream'):null;
 
 module.exports = function(array,deep){
   var txt,cons;
@@ -26,6 +29,214 @@ module.exports = function(array,deep){
       }),{sync: true});
       
     })(i);
+    
+  });
+  
+  new Test('ArrayBuffer',function(test){
+    
+    for(i = 0;i < array.length;i++) (function(i){
+      
+      ebjs.pack(array[i],test.wrap(function(data){
+        assert(data instanceof ArrayBuffer);
+        ebjs.unpack(data,test.wrap(function(result){
+          if(deep) assert.deepEqual(result,array[i]);
+          else assert.strictEqual(result,array[i]);
+        }),{sync: true});
+      }),{sync: true,type: 'arraybuffer'});
+      
+    })(i);
+    
+  });
+  
+  new Test('base64',function(test){
+    
+    for(i = 0;i < array.length;i++) (function(i){
+      
+      ebjs.pack(array[i],test.wrap(function(data){
+        assert.strictEqual(data.constructor,String);
+        ebjs.unpack(data,test.wrap(function(result){
+          if(deep) assert.deepEqual(result,array[i]);
+          else assert.strictEqual(result,array[i]);
+        }),{sync: true});
+      }),{sync: true,type: 'base64'});
+      
+    })(i);
+    
+  });
+  
+  new Test('dataurl',function(test){
+    
+    for(i = 0;i < array.length;i++) (function(i){
+      
+      ebjs.pack(array[i],test.wrap(function(data){
+        assert.strictEqual(data.constructor,String);
+        ebjs.unpack(data,test.wrap(function(result){
+          if(deep) assert.deepEqual(result,array[i]);
+          else assert.strictEqual(result,array[i]);
+        }),{sync: true});
+      }),{sync: true,type: 'dataurl'});
+      
+    })(i);
+    
+  });
+  
+  if(Stream) new Test('Stream',function(test){
+    
+    for(i = 0;i < array.length;i++) (function(i){
+      
+      ebjs.pack(array[i],test.wrap(function(data){
+        assert(data instanceof Stream);
+        ebjs.unpack(data,test.wrap(function(result){
+          if(deep) assert.deepEqual(result,array[i]);
+          else assert.strictEqual(result,array[i]);
+        }),{sync: true});
+      }),{sync: true,target: new Stream.PassThrough()});
+      
+    })(i);
+    
+  });
+  
+  new Test('vzBuffer sync',function(test){
+    
+    new Test(txt,function(test){
+      
+      for(i = 0;i < array.length;i++) (function(i){
+        
+        ebjs.pack(array[i],test.wrap(function(data){
+          assert(data instanceof vzBuffer);
+          
+          ebjs.unpack(data,test.wrap(function(result){
+            if(deep) assert.deepEqual(result,array[i]);
+            else assert.strictEqual(result,array[i]);
+          }),{sync: true,allowBlobs: true});
+        }),{sync: true,target: new vzBuffer()});
+        
+      })(i);
+      
+    });
+    
+    new Test('ArrayBuffer',function(test){
+      
+      for(i = 0;i < array.length;i++) (function(i){
+        
+        ebjs.pack(array[i],test.wrap(function(data){
+          assert(data instanceof vzBuffer);
+          
+          ebjs.unpack(data,test.wrap(function(result){
+            if(deep) assert.deepEqual(result,array[i]);
+            else assert.strictEqual(result,array[i]);
+          }),{sync: true});
+        }),{sync: true,target: new vzBuffer(),type: 'arraybuffer'});
+        
+      })(i);
+      
+    });
+    
+    new Test('base64',function(test){
+      
+      for(i = 0;i < array.length;i++) (function(i){
+        
+        ebjs.pack(array[i],test.wrap(function(data){
+          assert(data instanceof vzBuffer);
+          
+          ebjs.unpack(data,test.wrap(function(result){
+            if(deep) assert.deepEqual(result,array[i]);
+            else assert.strictEqual(result,array[i]);
+          }),{sync: true});
+        }),{sync: true,target: new vzBuffer(),type: 'base64'});
+        
+      })(i);
+      
+    });
+    
+    new Test('dataurl',function(test){
+      
+      for(i = 0;i < array.length;i++) (function(i){
+        
+        ebjs.pack(array[i],test.wrap(function(data){
+          assert(data instanceof vzBuffer);
+          
+          ebjs.unpack(data,test.wrap(function(result){
+            if(deep) assert.deepEqual(result,array[i]);
+            else assert.strictEqual(result,array[i]);
+          }),{sync: true});
+        }),{sync: true,target: new vzBuffer(),type: 'dataurl'});
+        
+      })(i);
+      
+    });
+    
+  });
+  
+  new Test('vzBuffer async',function(test){
+    
+    new Test(txt,function(test){
+      
+      for(i = 0;i < array.length;i++) (function(i){
+        
+        ebjs.pack(array[i],test.wrap(function(data){
+          assert(data instanceof vzBuffer);
+          
+          ebjs.unpack(data,test.wrap(function(result){
+            if(deep) assert.deepEqual(result,array[i]);
+            else assert.strictEqual(result,array[i]);
+          }),{allowBlobs: true});
+        }),{target: new vzBuffer()});
+        
+      })(i);
+      
+    });
+    
+    new Test('ArrayBuffer',function(test){
+      
+      for(i = 0;i < array.length;i++) (function(i){
+        
+        ebjs.pack(array[i],test.wrap(function(data){
+          assert(data instanceof vzBuffer);
+          
+          ebjs.unpack(data,test.wrap(function(result){
+            if(deep) assert.deepEqual(result,array[i]);
+            else assert.strictEqual(result,array[i]);
+          }));
+        }),{target: new vzBuffer(),type: 'arraybuffer'});
+        
+      })(i);
+      
+    });
+    
+    new Test('base64',function(test){
+      
+      for(i = 0;i < array.length;i++) (function(i){
+        
+        ebjs.pack(array[i],test.wrap(function(data){
+          assert(data instanceof vzBuffer);
+          
+          ebjs.unpack(data,test.wrap(function(result){
+            if(deep) assert.deepEqual(result,array[i]);
+            else assert.strictEqual(result,array[i]);
+          }));
+        }),{target: new vzBuffer(),type: 'base64'});
+        
+      })(i);
+      
+    });
+    
+    new Test('dataurl',function(test){
+      
+      for(i = 0;i < array.length;i++) (function(i){
+        
+        ebjs.pack(array[i],test.wrap(function(data){
+          assert(data instanceof vzBuffer);
+          
+          ebjs.unpack(data,test.wrap(function(result){
+            if(deep) assert.deepEqual(result,array[i]);
+            else assert.strictEqual(result,array[i]);
+          }));
+        }),{target: new vzBuffer(),type: 'dataurl'});
+        
+      })(i);
+      
+    });
     
   });
   
